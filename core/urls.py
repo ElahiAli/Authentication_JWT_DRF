965277c1cfc33c11074bba8dacfdc200c8748fff
ElahiAli <ali_elahi00@outlook.com>
2022-10-19 18:53:44 +0330

    updated

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 router = DefaultRouter()
 router.register('users', views.UserView, basename='user')
