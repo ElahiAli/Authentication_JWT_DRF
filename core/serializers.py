fd69cd6853a7db89647c62fde682f00cf2a756fe
ElahiAli <ali_elahi00@outlook.com>
2023-02-14 09:59:43 +0330

    just checking the hook

--- core/serializers.py
+++ core/serializers.py
@@ -70,3 +70,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
+
+def check():
+    print("hook")
